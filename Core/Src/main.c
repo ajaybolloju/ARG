806372c1f1915dd0a0ab9e26f446a58a67a5430b
ajaybolloju <ajaybolloju@outlook.com>
2024-10-30 01:42:49 +0530

    UART2 added

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -7,7 +7,7 @@
   * @attention
   *
   * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
+  * All right s reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
@@ -45,6 +45,7 @@
 TIM_HandleTypeDef htim2;
 
 UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
@@ -55,6 +56,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 /* USER CODE END PFP */
 
@@ -104,9 +106,11 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM2_Init();
   MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
   HAL_UART_Receive_IT(&huart1, (uint8_t *) &aRxBuffer, 1);
+  HAL_UART_Receive_IT(&huart2, (uint8_t *) &aRxBuffer, 1);
 
   /* USER CODE END 2 */
 
@@ -119,7 +123,7 @@ int main(void)
     /* USER CODE BEGIN 3 */
 	  if(timeout_counter > 5)
 	  {
-		  printf("\n hi");
+		  HAL_UART_Transmit_IT(&huart2, (uint8_t *) "AT\r\n", (uint16_t)sizeof("AT\r\n"));
 		  timeout_counter = 0;
 		  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 	  }
@@ -243,6 +247,39 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -255,8 +292,8 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
@@ -295,6 +332,11 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *UartHandle)
   if (UartHandle->Instance == USART1)
   {
 	    HAL_UART_Receive_IT(&huart1, (uint8_t *) &aRxBuffer, 1);
+	    printf("%c", aRxBuffer);
+  }
+  if (UartHandle->Instance == USART2)
+  {
+	    HAL_UART_Receive_IT(&huart2, (uint8_t *) &aRxBuffer, 1);
 	    printf("%c", aRxBuffer);
   }
 }
